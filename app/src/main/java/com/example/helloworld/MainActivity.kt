a4bce769b54269e37fd46e2374156cc90920d588
Gianni Gabriel <gianni@momento.fm>
2023-01-24 00:01:56 -0500

    Add console logs

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.Scanner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("Hello, world!")
+        val name = "Gianni"
+        println("My name is $name")
     }
 }
